843f21b2a0edac07b51b19c61596afba127390ba
Reinhard Tartler <siretart@tauware.de>
2014-07-11 01:21:51 +0000

    Import x264_0.142.2431+gita5831aa.orig.tar.gz
    [dgit import orig x264_0.142.2431+gita5831aa.orig.tar.gz]

---
+++ version.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Script modified from upstream source for Debian packaging since packaging
+# won't include .git repository.
+echo '#define X264_VERSION " r2431 a5831aa"'
+echo '#define X264_POINTVER "0.142.2431 a5831aa"'
