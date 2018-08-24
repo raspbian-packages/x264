ecfe4ba8281d5bb6dbf2a7dee577eeb643c35c92
Rico Tzschichholz <ricotz@ubuntu.com>
2018-08-24 16:25:59 +0100

    Import x264_0.155.2917+git0a84d98.orig.tar.gz
    [dgit import orig x264_0.155.2917+git0a84d98.orig.tar.gz]

---
+++ version.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Script modified from upstream source for Debian packaging since packaging
+# won't include .git repository.
+echo '#define X264_VERSION " r2917 0a84d98"'
+echo '#define X264_POINTVER "0.155.2917 0a84d98"'
