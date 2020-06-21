0700a382f0afb743b512768ff064c168d90e35c4
Rico Tzschichholz <ricotz@ubuntu.com>
2020-06-21 14:52:08 +0100

    Import x264_0.159.2999+git296494a.orig.tar.gz
    [dgit import orig x264_0.159.2999+git296494a.orig.tar.gz]

---
+++ version.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Script modified from upstream source for Debian packaging since packaging
+# won't include .git repository.
+echo '#define X264_VERSION " r2999 296494a"'
+echo '#define X264_POINTVER "0.159.2999 296494a"'
