f262774650bd5628da5ccddc751325d076629c79
Rico Tzschichholz <ricotz@ubuntu.com>
2020-07-13 20:29:33 +0100

    Import x264_0.160.3011+gitcde9a93.orig.tar.gz
    [dgit import orig x264_0.160.3011+gitcde9a93.orig.tar.gz]

---
+++ version.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Script modified from upstream source for Debian packaging since packaging
+# won't include .git repository.
+echo '#define X264_VERSION " r3011 cde9a93"'
+echo '#define X264_POINTVER "0.160.3011 cde9a93"'
