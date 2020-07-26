d95fe7cc4b8c5cd05e02f353ea4c6f6733f6e3dc
Adam Borowski <kilobyte@angband.pl>
2020-07-26 15:52:56 +0100

    Properly detect x32.
    As usually, x32 gets mistaken for amd64 (both are x86-64 ABIs after all).
    This version doesn't use any assembly yet, a proper port is needed.
    
    Gbp-Pq: Name properly_detect_x32.patch

--- common/common.h
+++ common/common.h
@@ -802,7 +802,7 @@ static ALWAYS_INLINE int x264_predictor_clip( int16_t (*dst)[2], int16_t (*mvc)[
     return cnt;
 }
 
-#if ARCH_X86 || ARCH_X86_64
+#if ARCH_X86 || ARCH_X86_64 || ARCH_X32
 #include "x86/util.h"
 #endif
 
